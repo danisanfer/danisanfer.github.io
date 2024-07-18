b625846d8227d9fa05a19433323d1f2de6e7b810
danisanfer <dasanfer8@gmail.com>
2024-07-18 20:06:45 +0200

    Nueva web de curriculum

---
+++ db/db.js
@@ -0,0 +1,236 @@
+export default {
+  languages: [
+    {
+      title: "Spanish",
+      skillName: "Español (Nativo)",
+      color: "2",
+      percentage: "100",
+    },
+    {
+      title: "English",
+      skillName: "English (B2 - MCER)",
+      color: "1",
+      percentage: "80",
+    },
+    {
+      title: "Catalan",
+      skillName: "Valenci&aagrave;",
+      color: "1",
+      percentage: "95",
+    }
+  ],
+  projects: {
+    web: [
+      {
+        projectName: "danisanfer | Desarrollador web",
+        image: "images/cv-linkscode.png",
+        summary: "Una One-Page sencilla y adaptada a mis redes y proyectos como desarrollador web",
+        preview: "https://danisanfer.github.io/",
+        techStack: [
+          "HTML",
+          "Bootstrap",
+          "JavaScript",
+          "CSS...",
+        ],
+      },
+      {
+        projectName: "danisanfer | Desarrollador Web",
+        image: "images/cv-linkscode.png",
+        summary:
+          "Una One-Page sencilla y adaptada a mis redes y proyectos como desarrollador web.",
+        preview: "https://arturocode.github.io/links/",
+        techStack: [
+          "HTML",
+          "CSS",
+          "Bootstrap..."
+        ],
+      },
+      {
+        projectName: "danisanfer | Desarrollador Web",
+        image: "images/cv-linkscode.png",
+        summary:
+          "Una One-Page sencilla y adaptada a mis redes y proyectos como desarrollador web.",
+        preview: "https://arturocode.github.io/links/",
+        techStack: [
+          "HTML",
+          "CSS",
+          "Bootstrap..."
+        ],
+      },
+    ],
+    literature: [
+      {
+        projectName: "Portfolio",
+        image: "",
+        summary: "Portfolio",
+        preview: "",
+        techStack: ["HTML", "CSS", "JavaScript", "jQuery..."],
+      },
+      {
+        projectName: "Title",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [""],
+      },
+      {
+        projectName: "Landing Book Page",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [""],
+      }
+    ],
+    freelance: [
+      {
+        projectName: "",
+        image: "",
+        summary:
+          "",
+        preview: "",
+        techStack: [
+          "Node.js",
+          "JS",
+          "Express",
+          "HTML&CSS",
+          "MySQL...",
+        ],
+      },
+      {
+        projectName: "",
+        image: "",
+        summary: "",
+        preview: "",
+        techStack: [
+          "HTML",
+          "CSS",
+          "Bootstrap..."
+        ],
+      },
+      {
+        projectName: "Tu-Wiki",
+        image: "",
+        summary: "Próximamente.",
+        preview: "#",
+        techStack: [
+          "MongoDB",
+          "Express",
+          "Angular",
+          "node.js..."
+        ],
+      },
+    ],
+  },
+  experience: [
+    {
+      title: "Unae Digital",
+      duration: "  - actualidad",
+      subtitle: "2013 - Desarrollador web e-commerce",
+      details: [
+        "Desarrollador web especializado en desarrollo software, consultoría, optimización y usabilidad web e-commerce en la agencia Unae Digital",
+      ],
+      tags: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Bootstrap",
+        "jQuery",
+        "PHP",
+        "PrestaShop",
+        "Shopify",
+      ],
+    },
+    {
+      title: "",
+      duration: "",
+      subtitle: "",
+      details: [
+        "",
+      ],
+      tags: [
+        "Windows",
+        "Java",
+        "Internet Explorer",
+        "Legalia",
+        "Deposito Digital D2",
+      ],
+    },
+    {
+      title: "",
+      duration: "",
+      subtitle: "",
+      details: [
+        "",
+      ],
+      tags: [
+        "Windows",
+        "Ubuntu",
+        "Thunderbird",
+        "Backup",
+        "Office",
+        "Soporte Técnico",
+        "Programas",
+      ],
+    },
+  ],
+  education: [
+    {
+      title: "Ingeniero Técnico en Informática de Gestión",
+      duration: "2009-2012",
+      subtitle: "Universidad de Alicante",
+      details: [
+        "",
+      ],
+      tags: [
+        "Algoritmos",
+        "Gestión de Proyectos",
+        "Programación Orientada a Objetos",
+        "Gráficos 3D",
+        "Bases de Datos",
+        "Sistemas Operativos",
+        "Redes",
+        "PHP",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "C++",
+        "C#",
+      ],
+    },
+    {
+      title: "Formación complementaria",
+      duration: "2013 - Actualidad",
+      subtitle: "Harvard, Pompeu Fabra, Product Hackers School, Futur0 school",
+      details: [
+        "Cursos de growth hacking, marketing y posicionamiento web y desarrollo de aplicaciones web",
+      ],
+      tags: [
+        "Tratamiento de imágenes",
+        "SEO",
+        "Three.js",
+        "Growth Hacking",
+        "CRO",
+        "Python",
+        "Django",
+        "React",
+        "Git",
+        "Selenium",
+      ],
+    },
+    {
+      title: "",
+      duration: "",
+      subtitle: "",
+      details: [
+        "",
+      ],
+      tags: [
+        "",
+        "",
+        "",
+        "",
+        "",
+      ],
+    },
+  ],
+};
